7e71511236e70a09c7cce43f15fb65fa6b15e4ba
Elizaveta Vygovskaia <101337015@georgebrown.ca>
2022-04-03 15:54:00 -0400

    new model

---
+++ GBCSporting_Flip_Framework/Models/Registration.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GBCSporting_Flip_Framework.Models
+{
+    public class Registration
+    {
+        [Range(1, 1000, ErrorMessage = "Please select a customer")]
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Please select a product")]
+        public int ProductId { get; set; }
+        public Product? Product { get; set; }
+        public string Slug => Customer?.FullName.Replace(' ', '-').ToLower();
+    }
+}
