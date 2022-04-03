acc221c6ec5cbd4277f5c6d0d3ddae7d6c1e3f9a
Elizaveta Vygovskaia <101337015@georgebrown.ca>
2022-04-03 15:51:26 -0400

    New controller. Haven't finished yet

---
+++ GBCSporting_Flip_Framework/Controllers/RegistrationController.cs
@@ -0,0 +1,46 @@
+﻿using GBCSporting_Flip_Framework.Models;
+using GBCSporting_Flip_Framework.Models.DataLayer;
+using GBCSporting_Flip_Framework.Models.DataLayer.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GBCSporting_Flip_Framework.Controllers
+{
+    public class RegistrationController : Controller
+
+    {
+        private Repository<Customer> data { get; set; }
+        public RegistrationController(GBCSportingContext ctx) => data = new Repository<Customer>(ctx);
+
+        [Route("[controller]s")]
+        public ViewResult Index()
+        {
+            var authors = data.List(new QueryOptions<Customer>
+            {
+                OrderBy = a => a.FirstName
+            });
+            return View(authors);
+        }
+
+        /*public IActionResult Registered(int id)
+        {
+            var customer = data.Get(id);
+            return GoToAuthorSearch(customer);
+        }
+
+        // private helper method
+        private RedirectToActionResult GoToAuthorSearch(Customer customer)
+        {
+            // store author search data in TempData and redirect
+            var search = new SearchData(TempData)
+            {
+                SearchTerm = customer.FullName,
+                Type = "customer"
+            };
+            return RedirectToAction("Search", "Product");
+        }
+        */
+
+
+    }
+}
